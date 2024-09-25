254d16b88222239d765aae502d6ae02be0f80b21
Tony Meehan <tonymeehan@users.noreply.github.com>
2024-09-25 13:29:26 -0500

    update

--- install-prereqs.tf
+++ install-prereqs.tf
@@ -56,3 +56,78 @@ resource "helm_release" "jaeger" {
     value = "true"
   }
 }
+
+### OpenTelemetry Collector
+
+resource "helm_release" "otel_collector" {
+  name       = "otel-collector"
+  namespace  = "monitoring"
+  chart      = "open-telemetry/opentelemetry-collector"
+
+  set {
+    name = "image.repository"
+    value = "otel/opentelemetry-collector-k8s"
+  }
+
+  set {
+    name = "mode"
+    value = "deployment"
+  }
+
+  values = [
+    <<EOF
+resources:
+  limits:
+    memory: "500Mi"
+  requests:
+    memory: "500Mi"
+
+config:
+  extensions:
+    zpages:
+      endpoint: "localhost:55679"
+  receivers:
+    otlp:
+      protocols:
+        grpc:
+          endpoint: "localhost:4317"
+        http:
+          endpoint: "localhost:4318"
+    jaeger:
+      protocols:
+        thrift_http:
+          endpoint: "localhost:14268"
+  processors:
+    batch: {}
+  exporters:
+    otlp:
+      endpoint: "localhost:4319"
+    jaeger:
+      endpoint: http://jaeger-collector.monitoring.svc.cluster.local:14268/api/traces
+  service:
+    pipelines:
+      logs:
+        exporters:
+        - otlp
+        processors:
+        - batch
+        receivers:
+        - otlp
+      metrics:
+        exporters:
+        - otlp
+        processors:
+        - batch
+        receivers:
+        - otlp
+      traces:
+        exporters:
+        - otlp
+        processors:
+        - batch
+        receivers:
+        - otlp
+        - jaeger
+EOF
+  ]
+}
