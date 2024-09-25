6924f0718f10d3a869c1e15e583f0a2ace9b5c37
Tony Meehan <tonymeehan@users.noreply.github.com>
2024-09-25 14:52:01 -0500

    update

--- install-prereqs.tf
+++ install-prereqs.tf
@@ -14,7 +14,6 @@ resource "helm_release" "cert_manager" {
   name       = "cert-manager"
   namespace  = "cert-manager"
   create_namespace = true
-
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
   version    = "v1.15.2" 
@@ -31,13 +30,30 @@ resource "helm_release" "prometheus" {
   name       = "prometheus"
   namespace  = "monitoring"
   create_namespace = true
-
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-
   set {
     name  = "server.persistentVolume.size"
     value = "8Gi"
+  }
+}
+
+### Jaeger
+
+resource "helm_release" "jaeger" {
+  name       = "jaeger"
+  namespace  = "monitoring"
+  create_namespace = true
+  repository = "https://jaegertracing.github.io/helm-charts"
+  chart      = "jaeger"
+
+  depends_on = [
+    helm_release.prometheus
+  ]
+
+  set {
+    name  = "ingress.enabled"
+    value = "true"
   }
 }
 
