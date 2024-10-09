28a6a3c53dd538630c3a7cb15ecc1ce3cb44ba23
Student <student@superorbital.io>
2024-10-09 15:03:59 +0000

    update install script

--- scenarios/scenario-03-cre-2024-007/install.sh
+++ scenarios/scenario-03-cre-2024-007/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 kubectl -n rabbitmq create configmap definitions --from-file=definitions.json
-kubectl -n rabbitmq apply -f ./rabbitmq.yaml
+kubectl -n rabbitmq apply -f ./rabbitmq-00.yaml
