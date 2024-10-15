e2c16bd9403f533f0cbf295950d0634ac59740f7
Tony Meehan <tonymeehan@users.noreply.github.com>
2024-10-14 22:00:14 -0500

    org/exceptions updates

--- setup/exceptions/install.sh
+++ setup/exceptions/install.sh
@@ -2,6 +2,9 @@
 
 set -x
 
-curl -XPOST -H "Authorization: Bearer $TOKEN" "https://api-beta.prequel.dev:8080/v1/api/exceptions" -d @exceptions-org1-default.json
-curl -XPOST -H "Authorization: Bearer $TOKEN" "https://api-beta.prequel.dev:8080/v1/api/exceptions" -d @exceptions-org1-kube-system.json
-curl -XPOST -H "Authorization: Bearer $TOKEN" "https://api-beta.prequel.dev:8080/v1/api/exceptions" -d @exceptions-org1-prom-stack.json
+ORG=$1
+
+curl -XPOST -H "Authorization: Bearer $TOKEN" "https://api-beta.prequel.dev:8080/v1/api/exceptions" -d @exceptions-$ORG-default.json
+curl -XPOST -H "Authorization: Bearer $TOKEN" "https://api-beta.prequel.dev:8080/v1/api/exceptions" -d @exceptions-$ORG-kube-system.json
+curl -XPOST -H "Authorization: Bearer $TOKEN" "https://api-beta.prequel.dev:8080/v1/api/exceptions" -d @exceptions-$ORG-prequel.json
+curl -XPOST -H "Authorization: Bearer $TOKEN" "https://api-beta.prequel.dev:8080/v1/api/exceptions" -d @exceptions-$ORG-prom-stack.json
